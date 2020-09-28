3f47fa49df91014c1650642f3eeda55eee211a28
Drakonega <71513389+Drakonega@users.noreply.github.com>
2020-09-28 13:07:01 +0300

    Add files via upload
    File containing class Vector

---
+++ vector.py
@@ -0,0 +1,12 @@
+class Vector:
+
+    def __init__(self, x, y):
+        """Constructor"""
+        self.x = x
+        self.y = y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
