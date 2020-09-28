aabd1b63af0162cd43eb0cf734b17bc20a50ae74
Drakonega <71513389+Drakonega@users.noreply.github.com>
2020-09-28 14:15:15 +0300

    Update vector.py
    Added module and multiply functions

--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+from math import *
+
 class Vector:
 
     def __init__(self, x, y):
@@ -10,3 +12,12 @@ class Vector:
 
     def get_y(self):
         return self.y
+
+    def multiply(self, factor):
+        self.x *= factor
+        self.y *= factor
+        return
+
+    def module(self):
+        mod = pow(pow(self.x, 2) + pow(self.x, 2), 0.5)
+        return mod
