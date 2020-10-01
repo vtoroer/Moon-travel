78b5527ad726d8e3e5cca0cea2a56df08edf3fc1
B1llyJean <71513266+B1llyJean@users.noreply.github.com>
2020-10-01 21:41:10 +0300

    Create ActivePart.py

---
+++ ActivePart.py
@@ -0,0 +1,6 @@
+class ActivePart:
+  def __init__(self, raw_mass, fuel_mass, thrust, specific_impelse)
+    self.raw_mass = raw_mass
+    self.fuel_mass = fuel_mass
+    self.thrust = thrust
+    self.fuel_burned_per_second = thrust / specific_impelse
