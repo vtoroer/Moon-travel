6205c803b13e165cf5129fcc590c198874dfdd42
B1llyJean <71513266+B1llyJean@users.noreply.github.com>
2020-10-10 18:41:39 +0300

    Update ActivePart.py

--- ActivePart.py
+++ ActivePart.py
@@ -1,6 +1,9 @@
+import math.pi
+
 class ActivePart:
-  def __init__(self, raw_mass, fuel_mass, thrust, specific_impelse)
+  def __init__(self, raw_mass, fuel_mass, thrust, specific_impelse, diameter)
     self.raw_mass = raw_mass
     self.fuel_mass = fuel_mass
     self.thrust = thrust
     self.fuel_burned_per_second = thrust / specific_impelse
+    self.active_area = math.pi * diameter^2
