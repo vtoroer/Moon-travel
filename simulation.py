ae77f48c2940b3a237a631c14869809c95341f67
Drakonega <71513389+Drakonega@users.noreply.github.com>
2020-10-10 13:24:33 +0300

    Add files via upload

---
+++ simulation.py
@@ -0,0 +1,12 @@
+from plotting import *
+
+
+def run_simulation():
+
+    total_sim_steps = 0
+    moon_lander_steps = 0
+    command_module_steps = 0
+
+    final_plotting(total_sim_steps, moon_lander_steps, command_module_steps)
+
+    return 0
