ec806ed687462e9d2eedddb2f66fdac697796773
Drakonega <71513389+Drakonega@users.noreply.github.com>
2020-10-11 10:39:59 +0300

    Update force.py

--- force.py
+++ force.py
@@ -1,21 +1,13 @@
 from vector import *
 from math import *
 
-
-class Force:
-    def __init__(self, x, y):
-        """Constructor"""
-        self.x = x
-        self.y = y
-
-
 # apply_gravity returns acceleration for a vehicle due to a gravitational force
 def apply_gravity(gravitational_coefficient, moon_vehicle):
-    vector = moon_vehicle.multiply(gravitational_coefficient / pow(moon_vehicle.module(), 3))
+    vector = moon_vehicle.multiply(gravitational_coefficient / pow(moon_vehicle.get_module(), 3))
     return vector
 
 
-# NOTE that vehicle orientation vector's module must be equal to zero
+# NOTE that vehicle orientation vector's module must be equal to one
 # apply_thrust returns acceleration for a vehicle due to a thrust
 def apply_thrust(vehicle_mass, thrust, vehicle_orientation):
     vector = vehicle_orientation.multiply(thrust / vehicle_mass)
@@ -26,5 +18,5 @@ def apply_thrust(vehicle_mass, thrust, vehicle_orientation):
 # apply_resistance returns acceleration for a vehicle due to an air resistance
 def apply_resistance(vehicle_mass, atmospheric_density, resistance_coefficient, vehicle_resistance_area, vehicle_speed):
     constant = 2 * resistance_coefficient * atmospheric_density * vehicle_resistance_area / vehicle_mass
-    vector = vehicle_speed.multiply(constant * vehicle_speed.module(), )
+    vector = vehicle_speed.multiply(constant * vehicle_speed.get_module(), )
     return vector
