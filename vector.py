9484c6cecf8249a919fea3bb8aee26e48290023b
Drakonega <71513389+Drakonega@users.noreply.github.com>
2020-10-11 10:34:15 +0300

    Update vector.py

--- vector.py
+++ vector.py
@@ -6,18 +6,38 @@ class Vector:
         """Constructor"""
         self.x = x
         self.y = y
+        self.phi = math.atan(y/x)
+        self.module = pow(x**2+y**2,0.5)
 
     def get_x(self):
         return self.x
 
     def get_y(self):
         return self.y
+    
+    def get_phi(self):
+        return self.phi
+    
+    def get_module(self):
+        return self.module
+    
+    def update_by_xy(self, x, y):
+        self.x = x
+        self.y = y
+        self.phi = math.atan(y/x)
+        self.module = pow(x**2+y**2,0.5)
+        
+    def update_by_phi_and_module(self, phi, module):
+        self.x = module * math.cos(phi)
+        self.y = module * math.sin(phi)
+        self.phi = phi
+        self.length = module
+        
 
     def multiply(self, factor):
         self.x *= factor
         self.y *= factor
         return
 
-    def module(self):
-        mod = pow(pow(self.x, 2) + pow(self.x, 2), 0.5)
-        return mod
+    def module(self):        
+        return self.length
