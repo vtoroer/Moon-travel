8b660296f2b16147916828e3ee908cb37a872680
vtoroer <59308134+vtoroer@users.noreply.github.com>
2020-11-02 19:51:52 +0300

    Update near_moon.py

--- near_moon.py
+++ near_moon.py
@@ -4,32 +4,38 @@ import numpy as np
 import math
 
 # к моменту начала с этим блоком мы уже должны находиться в СО, связанной с луной
-def aiming(ship, moon_position_x, moon_position_y, slope_speed_moon, moon_radius, moon_acceleration, t_step):
+def aiming(lunar_ship, disconnected_part, moon_position_x, moon_position_y, slope_speed_moon, moon_radius, moon_acceleration):
 # корабль, позиция необходимой точки на Луне, угловая скорость луны, радиус луны, g луны, временной шаг
     speed_needed_norm = 3 / 2
     speed_needed_tang = 1 / 2
-    radius_vector = Vector(ship.vechile_position_x, ship.vechile_position_y)
+    radius_vector = Vector(lunar_ship.vechile_position_x, lunar_ship.vechile_position_y)
     aim_speed = slope_speed_moon * moon_radius
     while radius_vector.get_module() > moon_radius:
-        angle_rv_speed = math.pi - math.acos(np.dot(radius_vector, ship.vechile_velocity()) \
-                                   / (radius_vector.get_module() * ship.vechile_velocity.get_module()))
+        angle_rv_speed = math.pi - math.acos(np.dot(radius_vector, lunar_ship.vechile_velocity()) \
+                                   / (radius_vector.get_module() * lunar_ship.vechile_velocity.get_module()))
         # угол между радиусом-вектором и скростью
-        speed_norm = ship.vechile_velosity.get_module() * math.cos(angle_rv_speed)
-        speed_tang = ship.vechile_velosity.get_module() * math.sin(angle_rv_speed) - aim_speed
+        angle_rv_orientation = math.pi - lunar_ship.vechile_orientation + radius_vector.get_phi()
+        # угол между радиусом вектором и ориентацией
+        speed_norm = lunar_ship.vechile_velosity.get_module() * math.cos(angle_rv_speed)
+        speed_tang = lunar_ship.vechile_velosity.get_module() * math.sin(angle_rv_speed) - aim_speed
         # измеряем тангенциальную скорость относительно поверхности луны
         # считаем примерное время, через которое мы окажется на Луне, следующая часть может быть изменена
         acceleration_norm = (moon_acceleration - (speed_tang ** 2) / radius_vector.get_module()) / 2
         time_lun_land = abs(speed_norm - speed_needed_norm) / acceleration_norm
         acceleration_tang = speed_tang * time_lun_land
-        ship.thrust = 
+        perfect_angle = math.atan()
         # TODO применить к ракете силу, обновить координаты
+        for i in range(10):
+            lunar_ship.rotation()
+            lunar_ship.apply_forces()
+            disconnected_part.apply_forces(False, disconnected_part.current_mass)
+        radius_vector.update_by_xy(lunar_ship.vechile_position_x, lunar_ship.vechile_position_y)
         
     if (speed_tang <= 1) and (speed_norm <= 3):
         print ("We're alive, man!")
     else:
         print ("Oh, sorry, my friend. Hope next time'd be better.")
-    radius_vector = none    
-# TODO учесть вращение Луны вокруг своей оси (или понять, почему этого не нужно делать)
+    radius_vector = none   
 # TODO после перехода в СО связанной с Землёй, рассчитать положение точки на Луне
 # TODO добавить проверку максимальной перегрузки
 
@@ -37,6 +43,7 @@ def wait_six_hours (ship_up, ship_moon, current_time, slope_speed_moon, moon_rad
     time_gone = 60*60*6
     orbita_radius = pow(ship_up.vechile_position_x**2 + ship_up.vechile_position_y**2, 0.5)
     # находим необходимые константы
+    
     angle_up = ship_up.vechile_velocity.get_module()*time_gone/orbita_radius
     ship_up.vechile_orientation += angle
     angle_1_1 = angle_up + ship_up.vechile_velocity.get_phi()
@@ -48,6 +55,7 @@ def wait_six_hours (ship_up, ship_moon, current_time, slope_speed_moon, moon_rad
     ship_up.vechile_orientation_y = radius_vector.get_y
     radius_vector_1 = none
     # обновляем информацию о летающей части
+    
     angle_moon = slope_speed_moon*time_left
     ship_moon.vechile_orientation += angle_moon
     angle_2_2 = angle_moon + ship_moon.vechile_velocity.get_phi()
