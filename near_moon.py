f825d3a0b7e3c24e50ee637211cee1da9f9c0aae
vtoroer <59308134+vtoroer@users.noreply.github.com>
2020-11-30 00:11:25 +0300

    Update near_moon.py

--- near_moon.py
+++ near_moon.py
@@ -4,57 +4,62 @@ import numpy as np
 import math
 
 # к моменту начала с этим блоком мы уже должны находиться в СО, связанной с луной
-def aiming(lunar_ship, disconnected_part, moon_position_x, moon_position_y, slope_speed_moon, moon_radius, moon_acceleration):
-# корабль, позиция необходимой точки на Луне, угловая скорость луны, радиус луны, g луны, временной шаг
-    speed_needed_norm = 3 / 2
-    speed_needed_tang = 1 / 2
-    radius_vector = Vector(lunar_ship.vechile_position_x, lunar_ship.vechile_position_y)
-    aim_speed = slope_speed_moon * moon_radius
-    while radius_vector.get_module() > moon_radius:
-        angle_rv_speed = math.pi - math.acos(np.dot(radius_vector, lunar_ship.vechile_velocity()) \
-                                   / (radius_vector.get_module() * lunar_ship.vechile_velocity.get_module()))
-        # угол между радиусом-вектором и скростью
-        angle_rv_orientation = math.pi - lunar_ship.vechile_orientation + radius_vector.get_phi()
-        # угол между радиусом вектором и ориентацией
-        speed_norm = lunar_ship.vechile_velosity.get_module() * math.cos(angle_rv_speed)
-        speed_tang = lunar_ship.vechile_velosity.get_module() * math.sin(angle_rv_speed) - aim_speed
-        # измеряем тангенциальную скорость относительно поверхности луны
-        # считаем примерное время, через которое мы окажется на Луне, следующая часть может быть изменена
-        acceleration_norm = (moon_acceleration - (speed_tang ** 2) / radius_vector.get_module()) / 2
-        time_lun_land = abs(speed_norm - speed_needed_norm) / acceleration_norm
-        acceleration_tang = speed_tang * time_lun_land
-        perfect_angle = math.atan()
-        # TODO применить к ракете силу, обновить координаты
-        for i in range(10):
-            lunar_ship.rotation()
-            lunar_ship.apply_forces()
-            disconnected_part.apply_forces(False, disconnected_part.current_mass)
-        radius_vector.update_by_xy(lunar_ship.vechile_position_x, lunar_ship.vechile_position_y)
-        
-    if (speed_tang <= 1) and (speed_norm <= 3):
-        print ("We're alive, man!")
-    else:
-        print ("Oh, sorry, my friend. Hope next time'd be better.")
-    radius_vector = none   
-# TODO после перехода в СО связанной с Землёй, рассчитать положение точки на Луне
-# TODO добавить проверку максимальной перегрузки
+# разворачиваем ракету "спиной" к скорости
+def turn_against_speed(ship):
+    cos_angle_speed_orien = ((ship.vehicle_velocity.get_x()*ship.vehicle_orientation.get_x()
+                            + ship.vehicle_velocity.get_y()*ship.vehicle_orientation.get_y())
+                            / (ship.vehicle_velocity.get_module() * ship.vehicle_orientation.get_module()))
+    epsilon = 0.01
+    print(ship.vehicle_position_x, ship.vehicle_position_y)
+    if abs(cos_angle_speed_orien + 1) > epsilon:
+       goal_ang = math.pi + ship.vehicle_velocity.get_phi()
+       if goal_ang > math.pi*3/2:
+          goal_ang -= math.pi*2
+       ship.rotation(0.05555555, goal_ang)
+    return ship
 
-def wait_six_hours (ship_up, ship_moon, current_time, slope_speed_moon, moon_radius):
-    time_gone = 60*60*6
-    orbita_radius = pow(ship_up.vechile_position_x**2 + ship_up.vechile_position_y**2, 0.5)
+# функция говорит, надо ли ещё тормозить, приравниваем её THRUST_ENABLED
+def get_lower_to_moon(ship):
+    radius_vector = Vector(ship.vehicle_position_x, ship.vehicle_position_y)
+    # вводим радиус-вектор, с началом в центре Луны и концом на ракете
+    moon_surf_dist = radius_vector.get_module() - moon_radius
+    ship = turn_against_speed(ship)
+    # тормозим до тех пор, пока не достигнем высоты орбиты не больше 45 км
+    radius_vector = None
+    if moon_surf_dist >= 45000: 
+        return True
+    else: return False
+
+# в случае успеха выводится 0
+def find_orbit(ship, moon_acceleration, phi_moon_aim):
+    radius_vector = Vector(ship.vehicle_position_x, ship.vehicle_position_y)
+    aim_vector = Vector(moon_radius*math.cos(phi_moon_aim), moon_radius*math.sin(phi_moon_aim))
+    # вводим радиус-вектор, с началом в центре Луны и концом на ракете
+    moon_surf_dist = radius_vector.get_module() - moon_radius
+    # с помощью формул из интернета считаем сколько времени будем падать при нулевой норм скорости
+    # и сколько времени нужно, чтобы сбросить тангенцальную скорость до 0
+    delta_v = math.sqrt(2*moon_acceleration*moon_surf_dist)
+    delta_t = ship.current_mass*3050/ship.thrust*(1-math.exp(-delta_v/3050))
+    angle_speed_tang = math.pi/2 - radius_vector.get_phi
+    tang_speed = abs(ship.vehicle_velocity.get_module()*math.cos(angle_speed_tang))
+    delta_tau = tang_speed * ship.current_mass / ship.thrust
+    aim_vector.update_by_phi_and_module(aim_vector.get_phi() + moon_angular_velocity*
+                                        (delta_t + delta_tau), moon_radius)
+    # посчитаем расстояние до цели с запасом в отрицательную сторону 
+    angle_aim_fall = aim_vector.get_phi() - radius_vector.get_phi()
+    if abs(angle_aim_fall) >= (2*math.pi):
+        if angle_aim_fall < -math.pi*2:
+            angle_aim_fall += math.pi*2
+        elif angle_aim_fall > math.pi*2:
+            angle_aim_fall -= math.pi*2
+    distance_aim_fall = moon_radius*angle_aim_fall
+    radius_vector = None
+    if (distance_aim_fall <= 100) and (distance_aim_fall >= 0):
+        return 0
+    else: return 1
+
+def waiting (ship_up, ship_moon, current_time, slope_speed_moon, moon_radius):
     # находим необходимые константы
-    
-    angle_up = ship_up.vechile_velocity.get_module()*time_gone/orbita_radius
-    ship_up.vechile_orientation += angle
-    angle_1_1 = angle_up + ship_up.vechile_velocity.get_phi()
-    ship_up.vechile_velocity.update_by_phi_and_module(angle_1_1, ship_up.vechile_velocity.get_module())
-    radius_vector_1 = Vector(ship_up.vechile_position_x, ship_up.vechile_position_y)
-    angle_up += radius_vector.get_phi()
-    radius_vector_1.update_by_phi_and_module(angle_1, orbita_radius)
-    ship_up.vechile_orientation_x = radius_vector.get_x
-    ship_up.vechile_orientation_y = radius_vector.get_y
-    radius_vector_1 = none
-    # обновляем информацию о летающей части
     
     angle_moon = slope_speed_moon*time_left
     ship_moon.vechile_orientation += angle_moon
@@ -67,9 +72,5 @@ def wait_six_hours (ship_up, ship_moon, current_time, slope_speed_moon, moon_rad
     ship_moon.vechile_orientation_y = radius_vector.get_y
     radius_vector_2 = none
     # обновляем информацию о части, сидящей на Луне
-    return current_time += time_gone, 
+    return current_time += time_gone 
     
-def left_the_moon (ship)
-# TODO подобрать идеальный момент, взлететь (оказывается, задача очень похожа не первую функцию)
-def pre_docking (ship)
-# возможно, эта функция не понадобится
